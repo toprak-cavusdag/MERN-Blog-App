16aedcfdd7615ffc79456ba3b2ab0e686bd17eca
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-08-23 09:06:46 +0300

    routers added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,48 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./component/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import AuthorPosts from "./pages/AuthorPosts";
+import Authors from "./pages/Authors";
+import UserProfile from "./pages/UserProfile";
+import CreatePost from "./pages/CreatePost";
+import CategoryPosts from "./pages/CategoryPosts";
+import Dashboard from "./pages/Dashboard";
+import EditPost from "./pages/EditPost";
+import DeletePost from "./pages/DeletePost";
+import Logout from "./pages/Logout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "post/:id", element: <PostDetail /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: "profile/id", element: <UserProfile /> },
+      { path: "authors", element: <Authors /> },
+      { path: "create", element: <CreatePost /> },
+      { path: "posts/categories/:id", element: <CategoryPosts /> },
+      { path: "posts/users/:id", element: <AuthorPosts /> },
+      { path: "post/:id/edit", element: <EditPost /> },
+      { path: "post/:id/delete", element: <DeletePost /> },
+      { path: "myposts/:id", element: <Dashboard /> },
+      { path: "logout", element: <Logout /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   </StrictMode>
 );
