9f85d3a66a3c44d6b036aa3ba897303b9fc7b523
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-09-02 08:42:35 +0300

    error page done

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <section className="error-page">
+      <div className="center">
+        <h2>Pag Not Found</h2>
+        <Link to="/" className="btn primary">Go Back Home</Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
