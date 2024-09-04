3210a2e51536c066a586652cc01520ee65dd775f
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-09-04 11:43:54 +0300

    categories and Authors done!

--- frontend/src/pages/AuthorPosts.jsx
+++ frontend/src/pages/AuthorPosts.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { DUMMY_POST } from "../constant/data";
+import PostItem from "../component/PostItem";
 
 const AuthorPosts = () => {
-  return <div>AuthorPosts</div>;
+  const [posts, setPosts] = useState(DUMMY_POST);
+  return (
+    <section className="posts">
+      {posts.length > 0 ? (
+        <div className="container posts__container">
+          {posts.map(({ id, thumbnail, authorID, category, desc, title }) => (
+            <PostItem
+              key={id}
+              postID={id}
+              thumbnail={thumbnail}
+              authorID={authorID}
+              category={category}
+              desc={desc}
+              title={title}
+            />
+          ))}
+        </div>
+      ) : (
+        <h2 className="center">No Posts Found!</h2>
+      )}
+    </section>
+  );
 };
 
 export default AuthorPosts;
--- frontend/src/pages/CategoryPosts.jsx
+++ frontend/src/pages/CategoryPosts.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { DUMMY_POST } from "../constant/data";
+import PostItem from "../component/PostItem";
 
 const CategoryPosts = () => {
-  return <div>CategoryPosts</div>;
+  const [posts, setPosts] = useState(DUMMY_POST);
+  return (
+    <section className="posts">
+      {posts.length > 0 ? (
+        <div className="container posts__container">
+          {posts.map(({ id, thumbnail, authorID, category, desc, title }) => (
+            <PostItem
+              key={id}
+              postID={id}
+              thumbnail={thumbnail}
+              authorID={authorID}
+              category={category}
+              desc={desc}
+              title={title}
+            />
+          ))}
+        </div>
+      ) : (
+        <h2 className="center">No Posts Found!</h2>
+      )}
+    </section>
+  );
 };
 
 export default CategoryPosts;
