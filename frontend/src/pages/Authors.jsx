392ed773cf5688f521ed8d0eff0afa50a58fa1b3
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-09-23 08:32:27 +0300

    Authors page done!

--- frontend/src/pages/Authors.jsx
+++ frontend/src/pages/Authors.jsx
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
-import { AUTHORS_DUMMY } from "../constant/data";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import apiClients from "../lib/apiRequest";
+import Loader from "../component/Loader";
 const Authors = () => {
-  const [authors, setAuthors] = useState(AUTHORS_DUMMY);
+  const [authors, setAuthors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAuthorHandle = async () => {
+    setIsLoading(true);
+    try {
+      const response = await apiClients.apiBaseUrl.get("/users");
+      setAuthors(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false)
+  };
+
+  useEffect(() => {
+    getAuthorHandle();
+  }, []);
+
+  if(isLoading){
+    return <Loader />
+  }
+
   return (
     <section className="authors">
       {authors.length > 0 ? (
-        <div className="container authors__container">
-          {authors?.map((item) => {
+        <div  className="container authors__container">
+          {authors?.map((item, index) => {
             return (
-              <Link className="author" key={item.id} to={`/posts/users/${item.id}`}>
+              <Link
+                className="author"
+                key={index}
+                to={`/posts/users/${item._id}`}
+              >
                 <div className="author__avatar">
-                  <img src={item.avatar} alt={`Image of ${item.name}`} />
+                  <img src={`${import.meta.env.VITE_APP_ASSETS_URL}/uploads/${item.avatar}`} alt={`Image of ${item.name}`} />
                 </div>
                 <div className="author__info">
                   <h4>{item.name}</h4>
