32f018907cf34d18a130ff9a646cb3498800bbed
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-09-24 19:10:24 +0300

    delete post done

--- frontend/src/pages/DeletePost.jsx
+++ frontend/src/pages/DeletePost.jsx
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { UserContext } from "../context/userContext";
 import apiClients from "../lib/apiRequest";
+import Loader from "../component/Loader";
 
 const DeletePost = ({ postID: id }) => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useContext(UserContext);
   const token = currentUser?.token;
 
@@ -17,25 +19,34 @@ const DeletePost = ({ postID: id }) => {
   }, [token, navigate]);
 
   const removePost = async (id) => {
+    setIsLoading(true);
     try {
       const response = await apiClients.apiBaseUrl.delete(`/posts/${id}`, {
         withCredentials: true,
         headers: { Authorization: `Bearer ${token}` },
       });
-  
+
       if (response.status === 200) {
         console.log("Post deleted successfully");
         if (location.pathname === `/myposts/${currentUser.id}`) {
-          window.location.reload(); 
+          window.location.reload();
         } else {
-          navigate("/"); 
+          navigate("/");
         }
       }
+      setIsLoading(false);
     } catch (error) {
-      console.error("Error deleting post:", error.response ? error.response.data : error.message);
+      console.error(
+        "Error deleting post:",
+        error.response ? error.response.data : error.message
+      );
     }
+    setIsLoading(false);
   };
-  
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <button className="btn sm danger" onClick={() => removePost(id)}>
